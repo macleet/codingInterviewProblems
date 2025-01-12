09d2691eb2c402a9b17c9d063633372f2b20d7d8
Mac <seemin0506@gmail.com>
2025-01-12 15:26:05 -0800

    Add implementation for replace() function

--- oneAway.js
+++ oneAway.js
@@ -14,16 +14,23 @@ pale, bake -> false
 */
 
 function oneAway(str1, str2) {
-  if (str1.length === str2.length) return replace(str1, str2);
+  if (str1.length === str2.length) return replace(str1.split(""), str2.split(""));
   else if (str1.length + 1 === str2.length) return insert(str2, str1);
   else if (str1.length === str2.length + 1) return insert(str1, str2);
   return false;
 }
   
 function replace(str1, str2) {
-  
+  let replaced = false;
+  for (let i = 0; i < str1.length; i++) {
+    if (!replaced && str1[i] !== str2[i]) replaced = true;
+    else if (replaced && str1[i] !== str2[i]) return false;
+  }
+  return true;
 }
 
 function insert(str1, str2) {
   
 }
+
+console.log(oneAway("pale", "payy"));
