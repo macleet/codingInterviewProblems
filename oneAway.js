68ee6ed137b47c81fb9deeeba97d2954eb0f1920
Mac Lee <seemin0506@gmail.com>
2025-01-13 11:47:56 -0800

    Fix insert() function: Increment relevant string index with instead of decrement when a difference is found between the two strings.

--- oneAway.js
+++ oneAway.js
@@ -15,8 +15,8 @@ pale, bake -> false
 
 function oneAway(str1, str2) {
   if (str1.length === str2.length) return replace(str1.split(""), str2.split(""));
-  else if (str1.length + 1 === str2.length) return insert(str2.split(""), str1.split(""));
-  else if (str1.length === str2.length + 1) return insert(str1.split(""), str2.split(""));
+  else if (str1.length + 1 === str2.length) return insert(str1.split(""), str2.split(""));
+  else if (str1.length === str2.length + 1) return insert(str2.split(""), str1.split(""));
   return false;
 }
   
@@ -31,11 +31,16 @@ function replace(str1, str2) {
 
 function insert(str1, str2) {
   let inserted = false;
-  for (let i = 0, j = 0; i < str1.length; i++, j++) {
-    if (str1[i] !== str2[j]) j++;
-    else if (inserted && str1[i] !== str2[j]) return false;
+  for (let i = 0, j = 0; i < str2.length; i++, j++) {
+    if (str1[i] === str2[j]) continue;
+
+    if (inserted) return false;
+    else {
+      inserted = true;
+      i--;
+    }
   }
   return true;
 }
 
-console.log(oneAway("pale", "pnale"));
+console.log(oneAway("pale", "paleed")); 
