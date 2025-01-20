0f3f2214231cbc6b6e9ff6c18974f528ac1d7d21
Mac <seemin0506@gmail.com>
2025-01-20 15:11:52 -0800

    Add function to check if linked list is a palindrome by reversing, cloning, and comparing equality.

--- linkedLists/palindrome.js
+++ linkedLists/palindrome.js
@@ -1,5 +1,42 @@
 /* Implement a function to check if a linked list is a palindrome.  */
 
-function palindrome() {
+const { Node, SinglyLinkedList } = require("./LinkedList");
 
+function reverseList(head) {
+  let newHead = null;
+  while (head) {
+    const newNode = new Node(head.value);
+    newNode.next = newHead;
+    newHead = newNode;
+    head = head.next;
+  }
+  return newHead;
 }
+
+function isEqual(h1, h2) {
+  while (h1 && h2) {
+    if (h1.value !== h2.value) return false;
+
+    h1 = h1.next;
+    h2 = h2.next;
+
+  }
+  return true;
+}
+
+function palindrome(head) {
+  const reversedHead = reverseList(head);
+  // SinglyLinkedList.print(reversedHead)
+  return isEqual(head, reversedHead);
+}
+
+const list = new SinglyLinkedList();
+list.insertEnd("t");
+list.insertEnd("a");
+list.insertEnd("c");
+list.insertEnd("o");
+list.insertEnd("c");
+list.insertEnd("a");
+list.insertEnd("t");
+list.print();
+console.log(palindrome(list.head));
